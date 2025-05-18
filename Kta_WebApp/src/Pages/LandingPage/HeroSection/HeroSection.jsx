3f4c9fa076663689fdaa282e6fbf616f43432c03
Rajat_Mishra <rajatmishra9411@gmail.com>
2025-05-18 18:29:08 +0530

    somechanges in herosection and headings of landingpages

--- Kta_WebApp/src/Pages/LandingPage/HeroSection/HeroSection.jsx
+++ Kta_WebApp/src/Pages/LandingPage/HeroSection/HeroSection.jsx
@@ -14,61 +14,78 @@ function HeroSection() {
         autoplay: true,
         speed: 4000,
         autoplaySpeed: 4000,
-        
-        
     };
     return (
-        <div className="slider-container" id='hero' >
-            <Slider {...settings}>
-                <div className="heroBannerFirst">
-                    <div className='heroBannerFirstBlock'>
-                        <div className='heroBannerFirstHeading'>
-                            A complete tile fixing solution
+        <>
+
+            <div className="slider-container" id='heroDesk' >
+                <Slider {...settings}>
+                    <div className="heroBannerFirst">
+                        <div className='heroBannerFirstBlock'>
+                            <div className='heroBannerFirstHeading'>
+                                A complete tile fixing solution
+                            </div>
+                            <div className='heroBannerFirstDetails'>
+                                High-quality materials and expert solutions for durability, aesthetics, and protection. <br />Trusted by professionals worldwide.
+                            </div>
+                            <div className='outline-button'>
+                                Explore Our Products
+                            </div>
                         </div>
-                        <div className='heroBannerFirstDetails'>
-                            High-quality materials and expert solutions for durability, aesthetics, and protection. <br />Trusted by professionals worldwide.
+
+
+                    </div>
+                    <div className="heroBannerSecond">
+                        <div className='heroBannerSecondBlock'>
+                            <div className='heroBannerSecondHeading'>
+                                Innovative Solutions <br /> in Ceramics, Stone & <br /> Surface Care
+                            </div>
+                            <div className='heroBannerSecondDetails'>
+                                High-quality materials and expert solutions for durability,
+                                <br /> aesthetics, and protection. Trusted by professionals worldwide.
+                            </div>
+                            <div className='outline-button'>
+                                Explore Our Products
+                            </div>
                         </div>
-                        <div className='outline-button'>
-                            Explore Our Products
-                        </div>
+
                     </div>
 
+                    <div className="heroBannerThird">
+                        <div className='heroBannerThirdBlock'>
+                            <div className='heroBannerThirdHeading'>
+                                Innovative Solutions <br /> in Ceramics, Stone & <br /> Surface Care
+                            </div>
+                            <div className='heroBannerThirdDetails'>
+                                High-quality materials and expert solutions for durability,
+                                <br /> aesthetics, and protection. Trusted by professionals worldwide.
+                            </div>
+                            <div className='outline-button'>
+                                Explore Our Products
+                            </div>
+                        </div>
 
-                </div>
-                <div className="heroBannerSecond">
-                    <div className='heroBannerSecondBlock'>
-                        <div className='heroBannerSecondHeading'>
-                        Innovative Solutions <br /> in Ceramics, Stone & <br /> Surface Care
-                        </div>
-                        <div className='heroBannerSecondDetails'>
-                        High-quality materials and expert solutions for durability,
-                        <br /> aesthetics, and protection. Trusted by professionals worldwide.
-                        </div>
-                        <div className='outline-button'>
-                            Explore Our Products
-                        </div>
                     </div>
 
-                </div>
-              
-                <div className="heroBannerThird">
-                    <div className='heroBannerThirdBlock'>
-                        <div className='heroBannerThirdHeading'>
-                        Innovative Solutions <br /> in Ceramics, Stone & <br /> Surface Care
-                        </div>
-                        <div className='heroBannerThirdDetails'>
-                        High-quality materials and expert solutions for durability,
-                        <br /> aesthetics, and protection. Trusted by professionals worldwide.
-                        </div>
-                        <div className='outline-button'>
-                            Explore Our Products
-                        </div>
-                    </div>
+                </Slider>
+            </div>
 
+            <div className="heroSectionMob" >
+                <div className='heroTextBlock'>
+
+                <div className="heroHeading">
+                    Innovative Solutions in Ceramics, Stone & Surface Care
+                </div>
+                <div className="heroDetails">
+                    High-quality materials and expert solutions for durability, aesthetics, and protection. Trusted by professionals worldwide.
                 </div>
 
-            </Slider>
-        </div>
+                <div className="outline-button">
+                    Explore Our Products
+                </div>
+                </div>
+            </div>
+        </>
     );
 }
 
