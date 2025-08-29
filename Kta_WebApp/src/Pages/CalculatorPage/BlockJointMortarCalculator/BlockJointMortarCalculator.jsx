9d1cc19ab2cde94b1a00c7a2d622e0cc9912db1c
SilverTheDeveloper <rajatmishra9411@gmail.com>
2025-08-29 16:58:39 +0530

    cal 90% design complete

--- Kta_WebApp/src/Pages/CalculatorPage/BlockJointMortarCalculator/BlockJointMortarCalculator.jsx
+++ Kta_WebApp/src/Pages/CalculatorPage/BlockJointMortarCalculator/BlockJointMortarCalculator.jsx
@@ -5,99 +5,131 @@ function BlockJointMortarCalculator() {
   return (
     <div>
 
-        <h3>bhai abhi kam chala le kal kardunga</h3>
-        <p>Joint Filler Coverage Calculator</p>
-        <p>A tool to estimate the right quantity of tile joint filler for your requirements.</p>
-        <div className={styles.calcWindow}>
-            
-       
+      <div className={styles.headingDiv}>
+        <div className={styles.Heading} >Joint Filler Coverage Calculator</div>
+        <div className={styles.subHeading}>A tool to estimate the right quantity of tile joint filler for your requirements.</div>
+      </div>
+
+      <div className={styles.calcWindow}>
+
+
         <div className={styles.leftSection}>
-            <div className={styles.firstBlock}>
-                <p>Enter Area, Tile
-Width & Length</p>   
-                 <div className={styles.InputDropdownBox}>
-                              <input
-                                type="number"
-                                placeholder="0000"
-                              
-                              />
-                              <div class={styles.Divider}></div>
-                              <select>
-                                <option value={"Sq.ft"}>Sq.ft</option>
-                                <option value={"Sq.m"}>Sq.m</option>
-                                <option value={"Acres"}>Acres</option>
-                              </select>
-                 </div>
+          <div className={styles.firstBlock}>
+            <p className={styles.fieldHeading}>Enter Area, Tile
+              Width & Length</p>
 
-                   <div className={styles.InputDropdownBox}>
-                              <input
-                                type="number"
-                                placeholder="0000"
-                              
-                              />
-                              <div class={styles.Divider}></div>
-                              <select>
-                                <option value={"Sq.ft"}>Sq.ft</option>
-                                <option value={"Sq.m"}>Sq.m</option>
-                                <option value={"Acres"}>Acres</option>
-                              </select>
-                 </div>
+            <div>
+              <label className={styles.fieldSubHeading} for="total_Area">Total Area</label>
+              <div className={styles.InputDropdownBox}>
+                <input
+                  type="number"
+                  id="total_Area"
+                  placeholder="0000"
 
-                   <div className={styles.InputDropdownBox}>
-                              <input
-                                type="number"
-                                placeholder="0000"
-                              
-                              />
-                              <div class={styles.Divider}></div>
-                              <select>
-                                <option value={"Sq.ft"}>Sq.ft</option>
-                                <option value={"Sq.m"}>Sq.m</option>
-                                <option value={"Acres"}>Acres</option>
-                              </select>
-                 </div> 
-
-                 
+                />
+                <div class={styles.Divider}></div>
+                <select>
+                  <option value={"Sq.ft"}>Sq.ft</option>
+                  <option value={"Sq.m"}>Sq.m</option>
+                  <option value={"Acres"}>Acres</option>
+                </select>
+              </div>
             </div>
 
-            <div className={styles.secondBlock}>
-                <p>Enter Joint Width & Tile Thickness</p>
-                <div className={styles.secondBlockInnerDiv}>
-                 <div className={styles.InputDropdownBox}>
-                              <input
-                                type="number"
-                                placeholder="0000"
-                              
-                              />
-                              <div class={styles.Divider}></div>
-                              <select>
-                                <option value={"Sq.ft"}>Sq.ft</option>
-                                <option value={"Sq.m"}>Sq.m</option>
-                                <option value={"Acres"}>Acres</option>
-                              </select>
-                 </div> 
-                  <div className={styles.InputDropdownBox}>
-                              <input
-                                type="number"
-                                placeholder="0000"
-                              
-                              />
-                              <div class={styles.Divider}></div>
-                              <select>
-                                <option value={"Sq.ft"}>Sq.ft</option>
-                                <option value={"Sq.m"}>Sq.m</option>
-                                <option value={"Acres"}>Acres</option>
-                              </select>
-                 </div> 
-                 </div>
+            <div>
+              <label className={styles.fieldSubHeading} for="tile_Width">Tile Width (1-5000)</label>
+              <div className={styles.InputDropdownBox}>
+                <input
+                  type="number"
+                  id="tile_Width"
+                  placeholder="0000"
+
+                />
+                <div class={styles.Divider}></div>
+                <select>
+                  <option value={"Sq.ft"}>Sq.ft</option>
+                  <option value={"Sq.m"}>Sq.m</option>
+                  <option value={"Acres"}>Acres</option>
+                </select>
+              </div>
             </div>
 
-            <button>submit</button>
+            <div>
+              <label className={styles.fieldSubHeading} for="tile_Length">Tile Length (1-5000)</label>
+              <div className={styles.InputDropdownBox}>
+                <input
+                  type="number"
+                  id="tile_Length"
+                  placeholder="0000"
+
+                />
+                <div class={styles.Divider}></div>
+                <select>
+                  <option value={"Sq.ft"}>Sq.ft</option>
+                  <option value={"Sq.m"}>Sq.m</option>
+                  <option value={"Acres"}>Acres</option>
+                </select>
+              </div>
+            </div>
+
+
+
+          </div>
+
+          <div className={styles.secondBlock}>
+
+            <p className={styles.fieldHeading}>Enter Joint Width & Tile Thickness</p>
+            <div className={styles.secondBlockInnerDiv}>
+              <div>
+                <label className={styles.fieldSubHeading} for="joint_Width">Joint Width (1-5000)</label>
+                <div className={styles.InputDropdownBox}>
+                  <input
+                    type="number"
+                    id="joint_Width"
+                    placeholder="0000"
+
+                  />
+                  <div class={styles.Divider}></div>
+                  <select>
+                    <option value={"Sq.ft"}>Sq.ft</option>
+                    <option value={"Sq.m"}>Sq.m</option>
+                    <option value={"Acres"}>Acres</option>
+                  </select>
+                </div>
+              </div>
+
+              <div>
+                <label className={styles.fieldSubHeading} for="tile_Thickness">Tile Thickness (1-5000)</label>
+                <div className={styles.InputDropdownBox}>
+                  <input
+                    type="number"
+                    id="tile_Thickness"
+                    placeholder="0000"
+
+                  />
+                  <div class={styles.Divider}></div>
+                  <select>
+                    <option value={"Sq.ft"}>Sq.ft</option>
+                    <option value={"Sq.m"}>Sq.m</option>
+                    <option value={"Acres"}>Acres</option>
+                  </select>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+          <button className={styles.subButton}>submit</button>
         </div>
         <div className={styles.rightSection}>
-            <p>Required Tile Joint Filler</p>
+          <div className={styles.subHeading}>Required Tile Joint Filler</div>
+
+          <div className={styles.beforeDesc}>
+            Fill in the requirement
+on the left <br /> to display the quantity required
+          </div>
         </div>
-         </div>
+      </div>
     </div>
   )
 }
